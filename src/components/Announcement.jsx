04916e9670d44f0efbc1b4e12052ddeb91cb3eb7
wala-beep <naghmouchiwala@gmail.com>
2022-09-03 10:26:33 +0100

    first commit

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Super Deal! Shipping is at 8 Dinars</Container>;
+};
+
+export default Announcement;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://img.freepik.com/free-photo/concept-perfume-isolated-white-background_185193-76658.jpg?w=2000",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://eco-beauty.dior.com/dw/image/v2/BDGF_PRD/on/demandware.static/-/Sites-master_dior/default/dwc339063b/assets/Y1996003/Y1996003_C099600310_E01_GHC.jpg?sw=715&sh=773&sm=fit&imwidth=800",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://eco-beauty.dior.com/dw/image/v2/BDGF_PRD/on/demandware.static/-/Sites-master_dior/default/dwcbcb7c29/assets/Y0996514/Y0996514_C099600521_E01_GHC.jpg?sw=460&sh=498&sm=fit&imwidth=460",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SKIN CARE",
+    },
+    {
+      id: 2,
+      img: "https://cdn.shopify.com/s/files/1/0225/0283/products/NO_model_web_1000x1000.png?v=1632844955",
+      title: "SHAMPOO & MASKS",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/c1/f5/c2/c1f5c2ae393d7b7226372d1204f0811e.jpg",
+      title: "PERFUME",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://eco-beauty.dior.com/dw/image/v2/BDGF_PRD/on/demandware.static/-/Sites-master_dior/default/dw15da2d1a/assets/Y0996059/Y0996059_C099600059_E01_GHC.jpg?sw=460&sh=498&sm=fit&imwidth=460",
+    },
+    {
+      id:2,
+      img:"https://png.pngtree.com/png-vector/20210531/ourlarge/pngtree-3d-beauty-cosmetics-product-design-png-image_3350325.jpg",
+    },
+    {
+      id:3,
+      img:"https://png.pngtree.com/png-clipart/20210606/original/pngtree-beauty-cosmetics-product-design-png-image_6391028.jpg",
+    },
+    {
+      id:4,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1608743236-d062c164-9b8f-4766-97f4-adeca2b84b7e.bcfa70bb528b0fbc5f501f9bb9c20fb7.jpg",
+    },
+    {
+      id:5,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1628545731-315K8TyZ6nL._SL500_.jpg?crop=0.667xw:1xh;center,top&resize=480:*",
+    },
+    {
+      id:6,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1656077185-cetaphil_gentle_skin_cleanser_236ml_1625562116_main.jpg?crop=0.667xw:1xh;center,top&resize=480:*",
+    },
+    {
+      id:7,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1605702395-tata-harper-cleanser-1605702392.jpg",
+    },
+    {
+      id:8,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1598299038-p427419-av-01-zoom.jpg?crop=0.667xw:1xh;center,top&resize=480:*",
+    },
+  ]
